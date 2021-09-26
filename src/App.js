334d2203b53c7d89d1beef6a22866f52c846bbbf
Andre Menezes <dehhmenezes87@gmail.com>
2021-09-26 17:35:57 -0300

    Drag and Drop

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from 'react';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import './App.css';
 import CardTask from './components/CardTask';
 import { ToDoContext } from './context';
@@ -14,56 +15,84 @@ export default function App() {
     document.getElementById('input-task').focus();
   }
 
+  const handleOnDragEnd = (result) => {
+    if (!result.destination) return;
+
+    const items = Array.from(list);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+    
+    setList(items);
+  }
+
   return (
-    <>
-      <header><h1>To Do List!</h1></header>
-      <main>
-        <form>
-          <label htmlFor="task">
-            Task:
-            <input
-              id="input-task"
-              onChange={({ target }) => setTask(target.value)}
-              type="text"
-              value={ task }
-            />
-          </label>
-          <button
-            onClick={ addTask }
-            type="button"
-          >
-            Add
-          </button>
-          { list.length ? (
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <main className="container">
+        <div>
+          <header><h1>To Do List!</h1></header>
+          <form className="input-area">
+            <label htmlFor="task">
+              Task:
+              <input
+                className='input-text'
+                id="input-task"
+                onChange={({ target }) => setTask(target.value)}
+                type="text"
+                value={ task }
+              />
+            </label>
             <button
-              onClick={ () => setList([]) }
+              onClick={ addTask }
               type="button"
             >
-              Clear List
+              Add
             </button>
-          ) : null }
-        </form>
-        <section>
+            { list.length ? (
+              <button
+                onClick={ () => setList([]) }
+                type="button"
+              >
+                Clear List
+              </button>
+            ) : null }
+          </form>
+        </div>
+        <section  className="output-area">
           {
             list ? (
-              <ul className="list">
-                {list.map((task, index) => (
-                  <CardTask
-                    currList={list}
-                    changeList={setList}
-                    key={task}
-                    task={task}
-                    index={index}
-                    last={list.length - 1}
-                  />
-                ))}
-              </ul>
-            )
-            : null
+                <Droppable droppableId="tasks">
+                  {(provided) => (
+                    <ol
+                      className="list"
+                      {...provided.droppableProps}
+                      ref={provided.innerRef}
+                    >
+                      {list.map((task, index) => (
+                        <Draggable key={task} index={index} draggableId={task}>
+                          {(provided) => (
+                            <li
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                            >
+                              <CardTask
+                                currList={list}
+                                changeList={setList}
+                                task={task}
+                              />
+                            </li>
+                          )}
+                        </Draggable>
+                      ))}
+                      {provided.placeholder}
+                    </ol>
+                  )}
+              </Droppable>
+            ) : null
           }
         </section>
       </main>
       <footer><p>2021 | Desenvolvido por André Menezes</p></footer>
-    </>
+    </DragDropContext>
   );
 };
--- src/components/CardTask.jsx
+++ src/components/CardTask.jsx
@@ -1,26 +1,18 @@
 import React, { useContext } from 'react';
 import { ToDoContext } from '../context';
-import arrowDown from '../images/arrow-down.png';
-import arrowUp from '../images/arrow-up.png';
 import edit from '../images/pencil.png';
 import remove from '../images/trash.png';
 
 export default function CardTask(props) {
-  const { task, index, last } = props;
+  const { task } = props;
   const { list, setList } = useContext(ToDoContext);
-  
-  const changePosition = (arr, from, to) => {
-    const getElement = arr.splice(to, 1, arr[from]);
-    arr[from] = getElement;
-    return setList(arr.flat());
-  }
+
 
   return (
-    <button
-      type="button"
+    <div
       className='tasks'
     >
-      <span>{`${index + 1}. ${task}`}</span>
+      <span>{task}</span>
       <div className="actions">
         <button
           className="action-button remove"
@@ -31,27 +23,7 @@ export default function CardTask(props) {
         <button className="action-button edit">
           <img src={ edit } alt="edit" />
         </button>
-        {
-          index < last ? (
-            <button
-              className="action-button"
-              onClick={ () => changePosition(list, index, index + 1) }
-            >
-              <img src={ arrowDown } alt="arrow-down" />
-            </button>
-          ) : null
-        }
-        {
-          index > 0 ? (
-          <button
-            className="action-button"
-            onClick={ () => changePosition(list, index, index - 1) }
-          >
-            <img src={ arrowUp } alt="arrow-up" />
-          </button>
-          ) : null
-        }
       </div>
-    </button>
+    </div>
   );
 };
